d04685bc85237a51a287a8ef453ba08766d9772f
Ahmed Rezk <work72019@gmail.com>
2020-05-04 23:21:17 -0600

    Activity Indicator

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
-import { FlatList } from "react-native";
+import { View, FlatList } from "react-native";
 import { ListItem, Tile } from "react-native-elements";
 import { connect } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
+import Loading from "./LoadingComponent";
 import { CAMPSITES } from "../shared/campsites";
 
 const mapStateToProps = (state) => {
@@ -39,6 +40,16 @@ class Directory extends Component {
 			);
 		};
 
+		if (this.props.campsites.isLoading) {
+			return <Loading />;
+		}
+		if (this.props.campsites.errMess) {
+			return (
+				<View>
+					<Text>{this.props.campsites.errMess}</Text>
+				</View>
+			);
+		}
 		return (
 			<FlatList
 				data={this.props.campsites.campsites}
