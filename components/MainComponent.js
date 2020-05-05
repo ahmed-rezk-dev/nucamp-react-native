8da299a0e20434676f0720945cf0c810078a5baf
Ahmed Rezk <work72019@gmail.com>
2020-05-04 23:32:39 -0600

    Forms

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -27,6 +27,7 @@ import {
 import SafeAreaView from "react-native-safe-area-view";
 import About from "./AboutComponent";
 import Contact from "./ContactComponent";
+import Reservation from "./ReservationComponent";
 
 const mapDispatchToProps = {
 	fetchCampsites,
@@ -192,6 +193,31 @@ const ContactNavigator = createStackNavigator(
 	}
 );
 
+const ReservationNavigator = createStackNavigator(
+	{
+		Reservation: { screen: Reservation },
+	},
+	{
+		navigationOptions: ({ navigation }) => ({
+			headerStyle: {
+				backgroundColor: "#5637DD",
+			},
+			headerTintColor: "#fff",
+			headerTitleStyle: {
+				color: "#fff",
+			},
+			headerLeft: (
+				<Icon
+					name="tree"
+					type="font-awesome"
+					iconStyle={styles.stackIcon}
+					onPress={() => navigation.toggleDrawer()}
+				/>
+			),
+		}),
+	}
+);
+
 const MainNavigator = createDrawerNavigator(
 	{
 		Home: {
@@ -213,6 +239,20 @@ const MainNavigator = createDrawerNavigator(
 				drawerIcon: ({ tintColor }) => (
 					<Icon
 						name="list"
+						type="font-awesome"
+						size={24}
+						color={tintColor}
+					/>
+				),
+			},
+		},
+		Reservation: {
+			screen: ReservationNavigator,
+			navigationOptions: {
+				drawerLabel: "Reserve Campsite",
+				drawerIcon: ({ tintColor }) => (
+					<Icon
+						name="tree"
 						type="font-awesome"
 						size={24}
 						color={tintColor}
