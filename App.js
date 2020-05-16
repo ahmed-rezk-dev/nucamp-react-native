87dbc8638ed7149401464a902f4be350df7303b3
Ahmed Rezk <work72019@gmail.com>
2020-05-16 02:12:51 -0600

    ersist Redux Store.

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { YellowBox } from "react-native";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/es/integration/react";
+import Loading from "./components/LoadingComponent";
 import { ConfigureStore } from "./redux/configureStore";
-const store = ConfigureStore();
+const { persistor, store } = ConfigureStore();
 import Main from "./components/MainComponent";
 YellowBox.ignoreWarnings(["Warning: ..."]);
 console.disableYellowBox = true;
@@ -10,7 +12,9 @@ console.disableYellowBox = true;
 export default function App() {
 	return (
 		<Provider store={store}>
-			<Main />
+			<PersistGate loading={<Loading />} persistor={persistor}>
+				<Main />
+			</PersistGate>
 		</Provider>
 	);
 }
